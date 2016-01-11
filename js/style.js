c5d00bc5564ebabfce8754fc41f638b5de163339
howard <howard.hhli@gmail.com>
2016-01-11 15:59:33 +0800

    added new content

---
+++ js/style.js
@@ -0,0 +1,12 @@
+  .home-center .off {
+    display: inline;
+  }
+  .home-center:hover .off {
+    display: none;
+  }
+  .home-center .on {
+    display: none;
+  }
+  .home-center:hover .on {
+    display: inline;
+  }
